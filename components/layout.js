c1e2fe3a6b8a4f6e4f973b727b5b5eb1486fa3c3
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-12-28 10:54:38 +0700

    :recycle: Refactor inline style and :sparkles: Support hide footer

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Layout as AntdLayout, Menu } from 'antd'
+import styled from '@emotion/styled'
 
-const { Header, Content, Footer } = AntdLayout
+const { Header, Content, Footer: UnstyledFooter } = AntdLayout
+
+const Footer = styled(UnstyledFooter)`
+    text-align: center;
+    background: #fefefe;
+    display: ${props => props.hideFooter ? 'none' : 'block'} !important;
+`
 
 export const Layout = props => (
     <AntdLayout className="layout">
@@ -21,6 +28,6 @@ export const Layout = props => (
         <Content style={{ padding: '30px 50px', minHeight: '90vh' }}>
             {props.children}
         </Content>
-        <Footer style={{ textAlign: 'center', background: '#fefefe' }}>WOW This is footer !</Footer>
+        <Footer hideFooter={props.hideFooter}>WOW This is footer !</Footer>
     </AntdLayout>
 )
