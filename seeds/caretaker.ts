0e3995fa84886ff73461de0efb584e6d133e86b2
Your Name <you@example.com>
2019-12-28 11:41:29 +0700

    feat: care taker seed

---
+++ seeds/caretaker.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    review: [],
+    name: "test1",
+    abititude: "abtitude 1",
+    personality: "open mind and adabtable to change",
+    occupation: "gevernment officer",
+    sepcies: [1, 2, 3, 4, 5, 6, 7]
+  },
+
+  {
+    review: [],
+    name: "test2",
+    abititude: "abtitude 1",
+    personality: "open mind and adabtable to change",
+    occupation: "gevernment officer",
+    sepcies: [1, 2, 3, 4, 5, 6, 7]
+  },
+  {
+    review: [],
+    name: "test3",
+    abititude: "abtitude 1",
+    personality: "open mind and adabtable to change",
+    occupation: "gevernment officer",
+    sepcies: [1, 2, 3, 4, 5, 6, 7]
+  }
+];
