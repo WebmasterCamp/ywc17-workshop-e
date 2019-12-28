173542c2ecf12ea57a9a710bda6845174510bcc6
Your Name <you@example.com>
2019-12-28 10:03:43 +0700

    feat: with emotion

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import useUserInfo from "../hooks/user";
+import styled from "@emotion/styled";
 
 const Home = () => {
+  const Button = styled.button`
+    color: green;
+  `;
   const [userInfo] = useUserInfo();
-  return <div>{userInfo.username}</div>;
+  return (
+    <div>
+      {userInfo.username}
+      <Button>Test</Button>
+    </div>
+  );
 };
 
 export default Home;
